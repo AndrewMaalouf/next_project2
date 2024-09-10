ebe240f147b87271633c52775f503c28f470c597
Andrew Maalouf <andrewmaalouf11@gmail.com>
2024-09-11 00:04:39 +0300

    fixing form and banner

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,10 +10,9 @@ const Banner = () => {
         src="/abstract.jpg"
         alt="banner-image"
         className="banner-image"
-        width="100"
-        height="600" 
-        // layout="responsive"
-      />      
+        layout="fill"
+        // objectFit="cover"
+      />
       <div className="overlay-text">
         <h1>Welcome to My Page</h1>
         <p>This is the banner section.</p>
